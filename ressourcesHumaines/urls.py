49f88ae880154f4926fee81b102b474bd53f8eff
awertuip211@gmail.com <azertuip211@gmail.com>
2025-09-04 13:58:53 +0000

    fix media

--- ressourcesHumaines/urls.py
+++ ressourcesHumaines/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     path('recrutements/',include("recrutements.urls")),
    path('attendances/',include("attendances.urls"))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
