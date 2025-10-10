e1208cdd1c9826e561801eb9aa889258f8c3dbd5
awertuip211@gmail.com <azertuip211@gmail.com>
2025-10-10 14:07:16 +0000

    dashboard perso add

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -20,127 +21,132 @@ from django.utils import timezone
 from recrutements.models import *
 # Create your views here.
 
-class Dasbord(LoginRequiredMixin,TemplateView):
-    template_name = "mainpage/dashbord.html"
+class Dasbord(LoginRequiredMixin,View):
     login_url = reverse_lazy("login")
 
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return render(request, 'mainpage/dashbord.html', self.get_context_data())
+        else:
+            return render(request, 'mainpage/dashbord_normal.html', self.get_context_data())
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         me = self.request.user
-        users = User.objects.all()
-        user_close = users.filter(is_active=False)
-        departement_cpt = Departements.objects.all()
-        employee_cpt = Employee.objects.all()
         employee_added = employee_cpt.filter(created_by = me)
         departement_added = departement_cpt.filter(created_by = me)
-        free = User.objects.filter(is_active=True, profil_employee__conges__status=True).distinct().count()
-        genres = User.objects.aggregate(
-            hommes=Count('id', filter=Q(gender='masculin')),
-            femmes=Count('id', filter=Q(gender='feminin'))
-        )
-        date = now().date()  # ou une date spécifique
-
-        present_ids = Abcence.objects.filter(date=date).values_list('employee_id', flat=True)
-
-        # Employés absents aujourd'hui = embauchés avant ou à cette date, actifs, mais pas dans les présents
-        employees_absent = Employee.objects.filter(
-            date_embauche__lte=date,
-            user__is_active=True
-        ).exclude(id__in=present_ids)
         try:
             if me.profil_employee:
-                absence = Abcence.objects.filter(employee=me.profil_employee, is_absent=True,date__month=now().month,date__year=now().year).count()
+                    absence = Abcence.objects.filter(employee=me.profil_employee, is_absent=True,date__month=now().month,date__year=now().year).count()
         except Exception as e:
             absence = 0
-        sessions = Session.objects.filter(expire_date__gte=timezone.now())
-        uid_list = []
-        for session in sessions:
-            data = session.get_decoded()
-            uid = data.get('_auth_user_id')
-            if uid:
-                uid_list.append(uid)
-        logged_in = User.objects.filter(id__in=uid_list, is_active=True).count()
+        if me.is_staff or me.is_superuser:
+            users = User.objects.all()
+            user_close = users.filter(is_active=False)
+            departement_cpt = Departements.objects.all()
+            employee_cpt = Employee.objects.all()
+            free = User.objects.filter(is_active=True, profil_employee__conges__status=True).distinct().count()
+            genres = User.objects.aggregate(
+                hommes=Count('id', filter=Q(gender='masculin')),
+                femmes=Count('id', filter=Q(gender='feminin'))
+            )
+            date = now().date()  # ou une date spécifique
 
-        qs = Abcence.objects.filter(
-            is_absent=True,
-            date__year=now().year
-        ).annotate(
-            month=ExtractMonth('date')
-        ).values('month').order_by('month').annotate(total=Count('id'))
-        qs2 = CongesRequest.objects.filter(
-            created_at__year=now().year
-        ).aggregate(
-            accepted=Count('id', filter=Q(status='accepted')),
-            refused=Count('id', filter=Q(status='refused')),
-            pending=Count('id', filter=Q(status='pending')),
-            aborted=Count('id', filter=Q(status='aborted'))
-        )
+            present_ids = Abcence.objects.filter(date=date).values_list('employee_id', flat=True)
 
-        french_months = {
-            1: "Janvier", 2: "Février", 3: "Mars", 4: "Avril",
-            5: "Mai", 6: "Juin", 7: "Juillet", 8: "Août",
-            9: "Septembre", 10: "Octobre", 11: "Novembre", 12: "Décembre"
-        }
+            # Employés absents aujourd'hui = embauchés avant ou à cette date, actifs, mais pas dans les présents
+            employees_absent = Employee.objects.filter(
+                date_embauche__lte=date,
+                user__is_active=True
+            ).exclude(id__in=present_ids)
+            sessions = Session.objects.filter(expire_date__gte=timezone.now())
+            uid_list = []
+            for session in sessions:
+                data = session.get_decoded()
+                uid = data.get('_auth_user_id')
+                if uid:
+                    uid_list.append(uid)
+            logged_in = User.objects.filter(id__in=uid_list, is_active=True).count()
 
-        result = {french_months[month]: 0 for month in range(1, 13)}
-        for entry in qs:
-            month_name = french_months.get(entry['month'], str(entry['month']))
-            result[month_name] = entry['total']
+            qs = Abcence.objects.filter(
+                is_absent=True,
+                date__year=now().year
+            ).annotate(
+                month=ExtractMonth('date')
+            ).values('month').order_by('month').annotate(total=Count('id'))
+            qs2 = CongesRequest.objects.filter(
+                created_at__year=now().year
+            ).aggregate(
+                accepted=Count('id', filter=Q(status='accepted')),
+                refused=Count('id', filter=Q(status='refused')),
+                pending=Count('id', filter=Q(status='pending')),
+                aborted=Count('id', filter=Q(status='aborted'))
+            )
 
-
-        payed_employees = Employee.objects.filter(
-            user__is_active=True,
-            payslips__month=now().month,
-            payslips__year=now().year
-        ).distinct().count()
-
-        total_employees = Employee.objects.filter(user__is_active=True).count()
-        unpayed_employees = total_employees - payed_employees
-
-        inactifs = user_close.count()
-        top_offres = Offre.objects.annotate(
-            postulation_count=Count('postulations')
-        ).order_by('-postulation_count')[:5]
-        
-        niveaux = Candidat.objects.values('niveau').annotate(count=Count('id'))        
-
-        statuts = Postulation.objects.values('statut').annotate(count=Count('id'))
-        context['top_offres']= {
-                "labels": [offre.titre for offre in top_offres],  # Supposant que le modèle Offre a un champ 'titre'
-                "values": [offre.postulation_count for offre in top_offres]
+            french_months = {
+                1: "Janvier", 2: "Février", 3: "Mars", 4: "Avril",
+                5: "Mai", 6: "Juin", 7: "Juillet", 8: "Août",
+                9: "Septembre", 10: "Octobre", 11: "Novembre", 12: "Décembre"
             }
-        context['statuts']={
-                "labels": [statut['statut'] for statut in statuts],
-                "values": [statut['count'] for statut in statuts]
+
+            result = {french_months[month]: 0 for month in range(1, 13)}
+            for entry in qs:
+                month_name = french_months.get(entry['month'], str(entry['month']))
+                result[month_name] = entry['total']
+
+
+            payed_employees = Employee.objects.filter(
+                user__is_active=True,
+                payslips__month=now().month,
+                payslips__year=now().year
+            ).distinct().count()
+
+            total_employees = Employee.objects.filter(user__is_active=True).count()
+            unpayed_employees = total_employees - payed_employees
+
+            inactifs = user_close.count()
+            top_offres = Offre.objects.annotate(
+                postulation_count=Count('postulations')
+            ).order_by('-postulation_count')[:5]
+            
+            niveaux = Candidat.objects.values('niveau').annotate(count=Count('id'))        
+
+            statuts = Postulation.objects.values('statut').annotate(count=Count('id'))
+            context['top_offres']= {
+                    "labels": [offre.titre for offre in top_offres],  # Supposant que le modèle Offre a un champ 'titre'
+                    "values": [offre.postulation_count for offre in top_offres]
+                }
+            context['statuts']={
+                    "labels": [statut['statut'] for statut in statuts],
+                    "values": [statut['count'] for statut in statuts]
+                }
+            context['niveaux']={
+                    "labels": [niveau['niveau'] for niveau in niveaux],
+                    "values": [niveau['count'] for niveau in niveaux]
+                }
+            context["general"] ={
+                "actif_users": users.count() - inactifs,
+                "inactif_user": inactifs,
+                "departemts": departement_cpt.count(),
+                "employees": employee_cpt.count(),
+                "hommes": genres.get('hommes', 0),
+                "femmes": genres.get('femmes', 0),
+                "disponible": users.count() - free - inactifs,
+                "absent_today": employees_absent.count(),
+                'absence_label': list(result.keys()),
+                'absence_data': list(result.values()),
+                'logged_in':logged_in,
+                'conges_stats': {
+                    'accepted': qs2.get('accepted', 0),
+                    'refused': qs2.get('refused', 0),
+                    'pending': qs2.get('pending', 0),
+                    'aborted': qs2.get('aborted', 0)
+                },
+                'payments_stat':{
+                "payed": payed_employees,
+                "unpayed": unpayed_employees
             }
-        context['niveaux']={
-                "labels": [niveau['niveau'] for niveau in niveaux],
-                "values": [niveau['count'] for niveau in niveaux]
             }
-        context["general"] ={
-            "actif_users": users.count() - inactifs,
-            "inactif_user": inactifs,
-            "departemts": departement_cpt.count(),
-            "employees": employee_cpt.count(),
-            "hommes": genres.get('hommes', 0),
-            "femmes": genres.get('femmes', 0),
-            "disponible": users.count() - free - inactifs,
-            "absent_today": employees_absent.count(),
-            'absence_label': list(result.keys()),
-            'absence_data': list(result.values()),
-            'logged_in':logged_in,
-            'conges_stats': {
-                'accepted': qs2.get('accepted', 0),
-                'refused': qs2.get('refused', 0),
-                'pending': qs2.get('pending', 0),
-                'aborted': qs2.get('aborted', 0)
-            },
-            'payments_stat':{
-            "payed": payed_employees,
-            "unpayed": unpayed_employees
-        }
-        }
-       
+
         context["prive"] = {
             "employees_added": employee_added.count(),
             "departement_added": departement_added.count(),
