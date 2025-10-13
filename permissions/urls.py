1e5185124d672b00395b780c5e5009845992e1c2
awertuip211@gmail.com <azertuip211@gmail.com>
2025-10-13 17:34:30 +0000

    permissions fix

--- permissions/urls.py
+++ permissions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from permissions.views import Permissions, get_permissions, get_users
+from permissions.views import Permissions, get_user_permissions, get_permissions, get_users
 
 
 urlpatterns = [
     path('getperms/', get_permissions,name='get_perms'),
     path('getusers/',get_users,name='get_users'),
-    path('',Permissions.as_view(),name='perms')
+    path('get_user_permissions/', get_user_permissions, name='get_user_permissions'),
+    path('',Permissions.as_view(),name='add_permissions')
 ]
