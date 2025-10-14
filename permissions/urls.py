b60e5ea47717ea0819508a7b3ba139f117ca4511
awertuip211@gmail.com <azertuip211@gmail.com>
2025-10-14 14:49:13 +0000

    add remove perms

--- permissions/urls.py
+++ permissions/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from permissions.views import Permissions, get_user_permissions, get_permissions, get_users
+from permissions.views import Permissions, get_user_permissions, get_permissions, get_users, remove_all_user_permissions, remove_user_permissions
 
 
 urlpatterns = [
     path('getperms/', get_permissions,name='get_perms'),
     path('getusers/',get_users,name='get_users'),
     path('get_user_permissions/', get_user_permissions, name='get_user_permissions'),
-    path('',Permissions.as_view(),name='add_permissions')
+    path('',Permissions.as_view(),name='add_permissions'),
+    path('remove_all_user_permissions/', remove_all_user_permissions, name='remove_all_user_permissions'),
+    path('remove_permissions/',remove_user_permissions,name='remove_user_permissions')
 ]
