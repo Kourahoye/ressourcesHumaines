5b9ee5fed63ed6ce9094405222167d3bd4c51145
kourahoye <azertuip211@gmail.com>
2026-01-23 17:18:34 +0000

    departement heads

--- departements/forms.py
+++ departements/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import Departements
+
+from employees.models import Employee
+from .models import DepartementHead, Departements
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Submit, Div
 
@@ -38,3 +40,78 @@ class DepartementsForm(forms.ModelForm):
         
         if len(self.cleaned_data['name']) < 4:
             raise forms.ValidationError("Le nom du departement est trop court.")
+
+
+
+class DepartementHeadFrom(forms.ModelForm):
+    # employee = forms.ModelChoiceField(
+    #     queryset=Employee.objects.none(),
+    #     widget=forms.Select(attrs={
+    #         'id':'id_employee',
+    #         'class': 'select select-info w-full',
+    #     })
+    # )
+    class Meta:
+        model = DepartementHead
+        fields = ['departement', 'employee', 'start_date']
+        widgets ={
+            'departement': forms.Select(attrs={
+                'id':'id_departement',
+                'class': 'select select-info w-full',
+            }),
+            'employee': forms.Select(attrs={
+                'id':'id_employee',
+                'class': 'select select-info w-full',
+            }),
+            'start_date': forms.DateInput(attrs={
+                'class': 'input input-info w-full',
+                'type': 'date',
+            }),
+        }
+        
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['employee'].queryset = Employee.objects.none()
+        if self.data.get('employee'):
+            self.fields['employee'].queryset = Employee.objects.filter(
+                pk=self.data.get('employee')
+            )
+        # Personnalisation du style d'erreur sur les champs
+        for field_name, field in self.fields.items():
+            existing_classes = field.widget.attrs.get('class', '')
+            error_class = 'border-red-600'
+            field.widget.attrs['class'] = f"{existing_classes} {error_class}" if self.errors.get(field_name) else existing_classes
+
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Div(
+                Field('departement'),
+                Field('employee'),
+                Field('start_date'),
+                # Submit('submit', 'Enregistrer', css_class="btn btn-sm btn-info"),
+                css_class="space-y-4"
+            )
+        )
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        departement = cleaned_data.get('departement')
+        active = cleaned_data.get('active', True)
+
+        if departement and active:
+            qs = DepartementHead.objects.filter(
+                departement=departement,
+                active=True
+            )
+
+            if self.instance.pk:
+                qs = qs.exclude(pk=self.instance.pk)
+
+            if qs.exists():
+                raise forms.ValidationError(
+                    "Il y a déjà un chef de département actif pour ce département."
+                )
+
+        return cleaned_data
+
--- departements/models.py
+++ departements/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+# from employees.models import Employee
 
 User = get_user_model()
 # Create your models here.
@@ -19,3 +20,34 @@ class Departements(models.Model):
 
     def get_absolute_url(self):
         return reverse('departements_profil', kwargs={'pk': self.pk})
+    
+    
+class DepartementHead(models.Model):
+    departement = models.ForeignKey(Departements,on_delete=models.CASCADE,related_name='departement_heads')
+    employee = models.ForeignKey("employees.Employee",on_delete=models.CASCADE,related_name='employee_departement_heads')
+    start_date = models.DateField()
+    end_date = models.DateField(null=True,blank=True)
+    active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name='departement_heads_cree')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    updated_by = models.ForeignKey(User,on_delete=models.CASCADE,related_name="departement_head_modifies")
+    
+    def __str__(self):
+        return f"{self.employee} - {self.departement}"
+    
+    class Meta:
+        permissions = [("assign_departement_head", "Can assign departement head")]
+        constraints = [
+            models.UniqueConstraint(
+                fields=['departement'],
+                condition=models.Q(active=True),
+                name='unique_active_departement_head',
+                violation_error_message="Il y a déjà un chef de département actif pour ce département.Desactivez l'ancien avant d'en ajouter un nouveau."
+            ),
+            models.CheckConstraint(
+                condition=models.Q(end_date__gte=models.F('start_date')),
+                name='end_date_after_start_date',violation_error_message="La date de fin ne peut pas être antérieure à la date de début."
+            )
+        ]
+    
